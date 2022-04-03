c092eef2194431bf7b2b49ee7161b89120e4ba07
Michael wang <xiagongbaozhou@gmail.com>
2022-04-03 12:58:36 +0800

    first

---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="tokyo_tm_home">
+    <div class="home_content">
+      <div class="avatar">
+        <div
+          class="image avatar_img"
+          :style="{ backgroundImage: 'url(' + src1 + ')' }"
+        ></div>
+      </div>
+      <!-- End .avatar -->
+      <div class="details">
+        <h3 class="name">Michael Wang</h3>
+        <p class="job">
+          人生得意须尽欢，莫使金樽空对月
+        </p>
+
+        <Social />
+      </div>
+      <!-- End .details -->
+    </div>
+  </div>
+</template>
+
+<script>
+  import Social from "./Social";
+  export default {
+    components: {
+      Social,
+    },
+    data() {
+      return {
+        src1: require("../assets/img/slider/1.jpg"),
+      };
+    },
+  };
+</script>
+
+<style lang="scss"></style>
---
+++ src/components/Social.vue
@@ -0,0 +1,47 @@
+<template>
+  <ul class="social">
+    <li v-for="(social, i) in socialList" :key="i">
+      <a :href="social.link" target="_blank" rel="noreferrer"
+        ><img class="svg" :src="social.src" alt="social"
+      /></a>
+    </li>
+  </ul>
+  <!-- End. social -->
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        socialList: [
+         /*  {
+            link: "https://www.facebook.com/",
+            src: require("../assets/img/svg/social/facebook.svg"),
+          }, */
+          /* {
+            link: "https://www.weibo.com/",
+            src: require("../assets/img/svg/social/weibo.svg"),
+          },
+          {
+            link: "https://qq.com/",
+            src: require("../assets/img/svg/social/qq.svg"),
+          },
+          {
+            link: "https://www.douyin.com/",
+            src: require("../assets/img/svg/social/tik-tok.svg"),
+          },
+          {
+            link: "https://www.wechat.com/",
+            src: require("../assets/img/svg/social/wechat.svg"),
+          },*/
+          {
+            link: "https://github.com/Didudia",
+            src: require("../assets/img/svg/social/github.svg"),
+          },
+        ],
+      };
+    },
+  };
+</script>
+
+<style lang="scss" scoped></style>
